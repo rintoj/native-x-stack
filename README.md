b71e83ce76802a699ec06346d19d2ed387aba50b
Rinto Jose <rintoj@gmail.com>
2021-02-02 19:47:16 +0530

    perf: Inital release

---
+++ README.md
@@ -0,0 +1,70 @@
+# native-x-stack
+
+[![semantic-release](https://img.shields.io/badge/%20%20%F0%9F%93%A6%F0%9F%9A%80-semantic--release-e10079.svg)](https://github.com/semantic-release/semantic-release)
+
+This modules helps you define stacked views in React Native application
+
+## Install
+
+### Yarn
+
+```sh
+yarn add native-x-stack
+```
+
+### NPM
+
+```sh
+npm install native-x-stack
+```
+
+## Usage
+
+```tsx
+import { Stack } from 'native-x-stack'
+
+function MyComponent() {
+  return <Stack fill>
+    {...}
+  </Stack>
+}
+```
+
+## API
+
+| Property                                      | Default Value | Usage                                                |
+| --------------------------------------------- | ------------- | ---------------------------------------------------- |
+| alignLeft?: boolean                           | false         | Align left horizontally                              |
+| alignCenter?: boolean                         | false         | Align center horizontally                            |
+| alignRight?: boolean                          | false         | Align right horizontally                             |
+| alignTop?: boolean                            | false         | Align top vertically                                 |
+| alignMiddle?: boolean                         | false         | Align middle vertically                              |
+| alignBottom?: boolean                         | false         | Align bottom vertically                              |
+| justifyAround?: boolean                       | false         | Justify around items in the stack                    |
+| justifyBetween?: boolean                      | false         | Move items towards the corner of the stack           |
+| reverse?: boolean                             | false         | Reverse the direction of flex                        |
+| stack?: ReactNode[]                           |               | Content of stack                                     |
+| horizontal?: boolean                          | false         | Stack items horizontally if true                     |
+| fill?: boolean                                | false         | Fill parent container                                |
+| fillHorizontal?: boolean                      | false         | Take the width of the container horizontally         |
+| wrap?: boolean                                | false         | Wrap content once reached the end of the current row |
+| zIndex?: number                               |               | Z-Index ordering                                     |
+| width?: number                                |               | Width of the stack                                   |
+| height?: number                               |               | Height of the stack                                  |
+| minWidth?: number                             |               | Min width of the stack                               |
+| minHeight?: number                            |               | Min height of the stack                              |
+| maxWidth?: number                             |               | Max width of the stack                               |
+| maxHeight?: number                            |               | Max height of the stack                              |
+| overflowVisible?: boolean                     | false         | Make content visible beyond content borders          |
+| onLayout?: (event: LayoutChangeEvent) => void | false         | Callback handler for layout changes                  |
+| style: ViewStyle                              |               | Additional style                                     |
+
+## Automatic Release
+
+Here is an example of the release type that will be done based on a commit messages:
+
+| Commit message          | Release type          |
+| ----------------------- | --------------------- |
+| fix(pencil): [comment]  | Patch Release         |
+| feat(pencil): [comment] | Minor Feature Release |
+| perf(pencil): [comment] | Major Feature Release |
---
+++ src/stack.tsx
@@ -0,0 +1,158 @@
+import { ContainerStyleProps, useContainerStyle } from 'native-x-theme'
+import React, { memo, ReactNode, useMemo } from 'react'
+import { LayoutChangeEvent, View, ViewStyle } from 'react-native'
+import { styles as s } from 'tachyons-react-native'
+
+export interface StackProps extends ContainerStyleProps {
+  alignLeft?: boolean
+  alignCenter?: boolean
+  alignRight?: boolean
+  alignTop?: boolean
+  alignMiddle?: boolean
+  alignBottom?: boolean
+  justifyAround?: boolean
+  justifyBetween?: boolean
+  reverse?: boolean
+  children?: ReactNode | ReactNode[]
+  horizontal?: boolean
+  fill?: boolean
+  fillHorizontal?: boolean
+  wrap?: boolean
+  zIndex?: number
+  width?: number
+  height?: number
+  minWidth?: number
+  minHeight?: number
+  maxWidth?: number
+  maxHeight?: number
+  onLayout?: (event: LayoutChangeEvent) => void
+  overflowVisible?: boolean
+  style?: ViewStyle
+}
+
+function calculateHorizontalAlignment({
+  alignLeft,
+  alignRight,
+  alignCenter,
+  horizontal,
+}: StackProps) {
+  if (alignLeft === true) {
+    return horizontal ? s.justifyStart : s.itemsStart
+  }
+  if (alignCenter !== false) {
+    return horizontal ? s.justifyCenter : s.itemsCenter
+  }
+  if (alignRight === true) {
+    return horizontal ? s.justifyEnd : s.itemsEnd
+  }
+  return null
+}
+
+function calculateVerticalAlignment({
+  alignTop,
+  alignBottom,
+  alignMiddle,
+  horizontal,
+}: StackProps) {
+  if (alignTop === true) {
+    return horizontal ? s.itemsStart : s.justifyStart
+  }
+  if (alignMiddle !== false) {
+    return horizontal ? s.itemsCenter : s.justifyCenter
+  }
+  if (alignBottom === true) {
+    return horizontal ? s.itemsEnd : s.justifyEnd
+  }
+  return null
+}
+
+export const Stack = memo((props: StackProps) => {
+  const {
+    alignLeft = false,
+    alignCenter = false,
+    alignRight = false,
+    alignTop = false,
+    alignMiddle = false,
+    alignBottom = false,
+    wrap = false,
+    reverse = false,
+    width,
+    height,
+    minWidth,
+    minHeight,
+    maxWidth,
+    maxHeight,
+    justifyAround,
+    justifyBetween,
+    children,
+    horizontal = false,
+    fill,
+    fillHorizontal,
+    zIndex,
+    onLayout,
+    overflowVisible = false,
+    style,
+  } = props
+
+  const directionStyle = useMemo(() => (horizontal ? [s.flexRow] : []), [horizontal])
+  const fillStyle = useMemo(
+    () => (fill ? [s.flex, s.w100, s.h100] : fillHorizontal ? [s.flexRow, s.w100] : []),
+    [fill, fillHorizontal],
+  )
+
+  const styles = useContainerStyle(props)
+  const containerStyle = useMemo(
+    () =>
+      [
+        ...directionStyle,
+        ...fillStyle,
+        zIndex != null ? { zIndex } : undefined,
+        width != null ? { width } : undefined,
+        height != null ? { height } : undefined,
+        minWidth != null ? { minWidth } : undefined,
+        minHeight != null ? { minHeight } : undefined,
+        maxWidth != null ? { maxWidth } : undefined,
+        maxHeight != null ? { maxHeight } : undefined,
+        reverse ? [horizontal ? s.flexRowReverse : s.flexColumnReverse] : undefined,
+        calculateHorizontalAlignment({ alignLeft, alignRight, alignCenter, horizontal }),
+        calculateVerticalAlignment({ alignTop, alignBottom, alignMiddle, horizontal }),
+        justifyAround ? s.justifyAround : null,
+        wrap ? s.flexWrap : null,
+        justifyBetween ? s.justifyBetween : null,
+        overflowVisible ? s.overflowVisible : s.overflowHidden,
+        ...styles,
+        style,
+      ].filter(i => i != null) as ViewStyle,
+    [
+      directionStyle,
+      fillStyle,
+      zIndex,
+      width,
+      height,
+      minWidth,
+      minHeight,
+      maxWidth,
+      maxHeight,
+      reverse,
+      horizontal,
+      alignLeft,
+      alignRight,
+      alignCenter,
+      alignTop,
+      alignBottom,
+      alignMiddle,
+      justifyAround,
+      wrap,
+      justifyBetween,
+      overflowVisible,
+      styles,
+      style,
+    ],
+  )
+
+  return (
+    <View style={containerStyle} onLayout={onLayout}>
+      {children}
+    </View>
+  )
+})
